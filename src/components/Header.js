a545d33ee19a6e399dc20123ced8cb9f88cf0f02
unknown <maciekhnat@gmail.com>
2019-09-03 19:21:21 +0200

    Added hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,39 @@
 import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="hamburger" onClick={handleClick}>
+                    <div className="hamburger-box">
+                    <span className="hamburger-box">
+                        <span className="hamburger-inner"></span>
+                    </span>
+                </div>
+            </div>
+            <nav className="navigation">
+                <ul className="navigation-list">
+                    <li className="navigation-item">
+                        Home
+                    </li>
+                    <li className="navigation-item">
+                        About me
+                    </li>
+                    <li className="navigation-item">
+                        My projects
+                    </li>
+                    <li className="navigation-item">
+                        Contact
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+
+const handleClick = () => {
+    document.querySelector('.hamburger').classList.toggle('active');
+    document.querySelector('.navigation').classList.toggle('navigation-active');
+}
+
+export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../Header';
 
 const Home = () => {
     return (
         <div className="home-container">
-            
+            <Header />
         </div>
     )
 }
