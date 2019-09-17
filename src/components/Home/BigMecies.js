b184d66d402b2b04e7e0378134bd5c8e52a778e3
unknown <maciekhnat@gmail.com>
2019-09-17 14:04:11 +0200

    fighting gh-pages paths

--- src/components/Home/BigMecies.js
+++ src/components/Home/BigMecies.js
@@ -4,9 +4,9 @@ const BigMecies = () => {
     return (
         <div className="image-container">
             <picture className="image-container">
-                <source srcSet="https://github.com/mecies/me/blob/gh-pages/images/cmaciekbw.png"
+                <source srcSet="images/cmaciekbw.png"
                     media="(min-width: 800px)" />
-                <img className="image" src="https://github.com/mecies/me/blob/gh-pages/images/cmaciekbw.png" alt="Maciej Hnat's photograph" />
+                <img className="image" src="images/cmaciekbw.png" alt="Maciej Hnat's photograph" />
             </picture>
         </div>             
     )
