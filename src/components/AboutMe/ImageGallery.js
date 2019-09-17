d8e01ee72a18875e98bcfcd9e755b778db8b7cb9
unknown <maciekhnat@gmail.com>
2019-09-17 13:58:04 +0200

    fixing

--- src/components/AboutMe/ImageGallery.js
+++ src/components/AboutMe/ImageGallery.js
@@ -3,9 +3,9 @@ import React from 'react';
 const ImageGallery = () => {
     return (
         <div className="image-gallery"> 
-            <img className="gal-image one" src="https://github.com/mecies/me/gh-pages/images/img_gal.jpg" alt="placeholder" />
-            <img className="gal-image two" src="https://github.com/mecies/me/gh-pages/images/img_gal.jpg" alt="placeholder" />
-            <img className="gal-image three" src="https://github.com/mecies/me/gh-pages/images/img_gal.jpg" alt="placeholder" />
+            <img className="gal-image one" src="https://github.com/mecies/me/blob/gh-pages/images/img_gal.jpg" alt="placeholder" />
+            <img className="gal-image two" src="https://github.com/mecies/me/blob/gh-pages/images/img_gal.jpg" alt="placeholder" />
+            <img className="gal-image three" src="https://github.com/mecies/me/blob/gh-pages/images/img_gal.jpg" alt="placeholder" />
         </div>
     )
 }
