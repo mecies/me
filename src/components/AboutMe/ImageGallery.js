979736f588ca94d2ea6e0da93ceed5e5642fc16f
unknown <maciekhnat@gmail.com>
2019-09-17 13:55:37 +0200

    fixing paths

--- src/components/AboutMe/ImageGallery.js
+++ src/components/AboutMe/ImageGallery.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ImageGallery = () => {
     return (
-        <div className="image-gallery">
-            <img className="gal-image one" src="https://github.com/mecies/me/tree/gh-pages/images/img_gal.jpg" alt="placeholder" />
-            <img className="gal-image two" src="https://github.com/mecies/me/tree/gh-pages/images/img_gal.jpg" alt="placeholder" />
-            <img className="gal-image three" src="https://github.com/mecies/me/tree/gh-pages/images/img_gal.jpg" alt="placeholder" />
+        <div className="image-gallery"> 
+            <img className="gal-image one" src="https://github.com/mecies/me/gh-pages/images/img_gal.jpg" alt="placeholder" />
+            <img className="gal-image two" src="https://github.com/mecies/me/gh-pages/images/img_gal.jpg" alt="placeholder" />
+            <img className="gal-image three" src="https://github.com/mecies/me/gh-pages/images/img_gal.jpg" alt="placeholder" />
         </div>
     )
 }
--- src/components/Home/BigMecies.js
+++ src/components/Home/BigMecies.js
@@ -4,9 +4,9 @@ const BigMecies = () => {
     return (
         <div className="image-container">
             <picture className="image-container">
-                <source srcSet="https://github.com/mecies/me/tree/gh-pages/images/cmaciekbw.png"
+                <source srcSet="https://github.com/mecies/me/gh-pages/images/cmaciekbw.png"
                     media="(min-width: 800px)" />
-                <img className="image" src="https://github.com/mecies/me/tree/gh-pages/images/cmaciekbw.png" alt="Maciej Hnat's photograph" />
+                <img className="image" src="https://github.com/mecies/me/gh-pages/images/cmaciekbw.png" alt="Maciej Hnat's photograph" />
             </picture>
         </div>             
     )
