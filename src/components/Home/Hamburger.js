2e0302fdad4f475114cb8e76cf6bb2fa343682cc
unknown <maciekhnat@gmail.com>
2019-09-19 11:05:24 +0200

    refactored hamburger to class base component

--- src/components/Home/Hamburger.js
+++ src/components/Home/Hamburger.js
@@ -1,42 +1,55 @@
 import React from 'react';
 
-const Hamburger = () => {
-    return (
-        <div>
-            <div className="hamburger" onClick={handleClick}>
-                <div className="hamburger-box">
-                    <span className="hamburger-box">
-                        <span className="hamburger-inner"></span>
-                    </span>
-                </div>
-            </div>
-            <nav className="navigation">
-                <ul className="navigation-list">
-                    <li className="navigation-item">
-                        <i className="fas fa-home"></i>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li className="navigation-item">
-                        <i className="fas fa-folder-open"></i>
-                        <a href="#myprojects">My projects</a>
-                    </li>
-                    <li className="navigation-item">
-                        <i className="fas fa-user"></i>
-                        <a href="#aboutme">About me</a>
-                    </li>
-                    <li className="navigation-item">
-                        <i className="fas fa-envelope"></i>
-                        <a href="#contactme">Contact me</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
-}
+class Hamburger extends React.Component {
 
-const handleClick = () => {
-    document.querySelector('.hamburger').classList.toggle('active');
-    document.querySelector('.navigation').classList.toggle('navigation-active');
+    handleClick = () => {
+        document.querySelector('.hamburger').classList.toggle('active');
+        document.querySelector('.navigation').classList.toggle('navigation-active');
+    }
+
+    
+
+    render() {
+        return (
+            <div>
+                <div className="hamburger" onClick={this.handleClick}>
+                    <div className="hamburger-box">
+                        <span className="hamburger-box">
+                            <span className="hamburger-inner"></span>
+                        </span>
+                    </div>
+                </div>
+                <nav className="navigation">
+                    <ul className="navigation-list">
+                        <li className="navigation-item">
+                            <a className="nav-anchor" href="#home">
+                                <i className="fas fa-home"></i>
+                                <span className="nav-span">Home</span>
+                            </a>
+                        </li>
+                        <li className="navigation-item">
+                            <a className="nav-anchor" href="#myprojects">
+                                <i className="fas fa-folder-open"></i>
+                                <span className="nav-span">My projects</span>
+                            </a>
+                        </li>
+                        <li className="navigation-item">
+                            <a className="nav-anchor" href="#aboutme">
+                                <i className="fas fa-user"></i>
+                                <span className="nav-span">About me</span>
+                            </a>
+                        </li>
+                        <li className="navigation-item">
+                            <a className="nav-anchor" href="#contactme">
+                                <i className="fas fa-envelope"></i>
+                                <span className="nav-span">Contact me</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
 }
 
 export default Hamburger;
