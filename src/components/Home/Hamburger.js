9ccb634016ffb51c4172f600bd19319793a39a6d
unknown <maciekhnat@gmail.com>
2019-09-19 11:09:06 +0200

    close menu when choose an option

--- src/components/Home/Hamburger.js
+++ src/components/Home/Hamburger.js
@@ -21,25 +21,25 @@ class Hamburger extends React.Component {
                 </div>
                 <nav className="navigation">
                     <ul className="navigation-list">
-                        <li className="navigation-item">
+                        <li className="navigation-item" onClick={this.handleClick}>
                             <a className="nav-anchor" href="#home">
                                 <i className="fas fa-home"></i>
                                 <span className="nav-span">Home</span>
                             </a>
                         </li>
-                        <li className="navigation-item">
+                        <li className="navigation-item" onClick={this.handleClick}>
                             <a className="nav-anchor" href="#myprojects">
                                 <i className="fas fa-folder-open"></i>
                                 <span className="nav-span">My projects</span>
                             </a>
                         </li>
-                        <li className="navigation-item">
+                        <li className="navigation-item" onClick={this.handleClick}>
                             <a className="nav-anchor" href="#aboutme">
                                 <i className="fas fa-user"></i>
                                 <span className="nav-span">About me</span>
                             </a>
                         </li>
-                        <li className="navigation-item">
+                        <li className="navigation-item" onClick={this.handleClick}>
                             <a className="nav-anchor" href="#contactme">
                                 <i className="fas fa-envelope"></i>
                                 <span className="nav-span">Contact me</span>
