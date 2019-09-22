0dd11ab496abe87cf5dbed5fa54466cbb1888f61
unknown <maciekhnat@gmail.com>
2019-09-22 20:26:46 +0200

    fixed contactme anchor and modified about sec

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
                 <div className="aboutme-left">
                     <Logo first="About me" second="" className="aboutme-logo"/>
                     <p className="aboutme-description">
-                        A web developer located in <a href="https://www.wroclaw.pl/" target="_blank" rel="noopener noreferrer">Wroclaw</a>. <br />
+                        A web developer located in <a href="https://www.wroclaw.pl/" target="_blank" rel="noopener noreferrer">Wroclaw</a>.
                         Currently studying Business Informatics at the <a href="http://www.ue.wroc.pl/" target="_blank" rel="noopener noreferrer">University of Economics</a> in Wroclaw. <b />
                         Dynamic visual changes keep me passionate about Frontend, however I am leaning towards Fullstack in the future.
                         Besides that I love traveling, art, board & video games and amateur powerlifting.
--- src/components/MyProjects/Preview.js
+++ src/components/MyProjects/Preview.js
@@ -4,7 +4,7 @@ const Preview = ({ previewContent }) => {
     console.log(previewContent)
     return (
         <div className="my-projects-preview">
-            <p className="project-idea">Have a cool project idea? <a href="#home">Contact me</a></p>
+            <p className="project-idea">Have a cool project idea? <a href="#contactme">Contact me</a></p>
             <p className="rec">{previewContent.title}</p>
             <div className="prev-icons">
                 <a href={previewContent.code}>Code: {previewContent.title === "Native Speaker" ? <i className="fas fa-dollar-sign"></i> : <i className="fas fa-code"></i>}</a>
