7b2f7309abeda53e732e7af0ecd3cde6a03bd485
unknown <maciekhnat@gmail.com>
2019-09-22 13:26:08 +0200

    added real about me images

--- src/components/AboutMe/ImageGallery.js
+++ src/components/AboutMe/ImageGallery.js
@@ -7,13 +7,13 @@ class ImageGallery extends React.Component {
         return (
             [
                 <div className="image-container" key="img1">
-                    <img className="gal-image" src="images/img_gal.jpg" alt="placeholder" />
+                    <img className="gal-image" src="images/clouds.jpg" alt="clouds" />
                 </div>,
                 <div className="image-container" key="img2">
-                    <img className="gal-image" src="images/img_gal.jpg" alt="placeholder" />
+                    <img className="gal-image" src="images/hangin.jpg" alt="placeholder" />
                 </div>,
                 <div className="image-container" key="img3">
-                    <img className="gal-image" src="images/img_gal.jpg" alt="placeholder" />
+                    <img className="gal-image" src="images/garden.jpg" alt="placeholder" />
                 </div>
             ]
         )
@@ -22,9 +22,9 @@ class ImageGallery extends React.Component {
     render() {
         return (
             <div className="image-gallery">
-                <img className="gal-image one" src="images/img_gal.jpg" alt="placeholder" />
-                <img className="gal-image two" src="images/img_gal.jpg" alt="placeholder" />
-                <img className="gal-image three" src="images/img_gal.jpg" alt="placeholder" />
+                <img className="gal-image one" src="images/clouds.jpg" alt="landscape of bieszczady mountains" />
+                <img className="gal-image two" src="images/hangin.jpg" alt="me and piece of art" />
+                <img className="gal-image three" src="images/garden.jpg" alt="me in the garden" />
                 <div className="slider-container">
                     <SimpleSlider show={this.renderImages()} />
                 </div>
