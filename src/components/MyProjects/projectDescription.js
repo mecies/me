b0dd9412963946b2e1353535af7422d42f758354
Maciej Hnat <39158460+mecies@users.noreply.github.com>
2021-02-06 19:18:53 +0100

    Update projectDescription.js

--- src/components/MyProjects/projectDescription.js
+++ src/components/MyProjects/projectDescription.js
@@ -5,8 +5,8 @@ export default {
     OnePL: { title: "Native Speaker", solution: "Projekt komercyjny - Zaprojektowałem stronę za pomocą Figmy. Zakodowałem frontend aplikacji w HAML/LESS i Javascript oraz podpiąłem go do istniejącego backendu w środowisku Ruby on Rails", code: "", live: "https://nativespeaker.com.pl/"},
     Two: { title: "Portfolio", solution: "My portfolio website designed with Figma and developed with HTML/CSS/ReactJS and couple of react-friendly libraries", code: "https://github.com/mecies/me", live: "https://mecies.github.io/me/" },
     TwoPL: { title: "Portfolio", solution: "Moja strona portfolio zaprojektowana przeze mnie w Figmie i zakodowana w HTML/CSS/ReactJS i kilkoma bibliotekami przyjaznymi Reactowi", code: "https://github.com/mecies/me", live: "https://mecies.github.io/me/" },
-    Three: { title: "Equarium", solution: "A website made for Student Organisation's project at my university. Written with regular HTML/CSS/JS + Webpack & Babel", code: "https://github.com/mecies/Equarium", live: "http://equarium.eu/" },
-    ThreePL: { title: "Equarium", solution: "Strona napisana dla projektu Organizacji Studenckiej na mojej uczelni w technologiach HTML/CSS/JS + Webpack i Babel", code: "https://github.com/mecies/Equarium", live: "http://equarium.eu/" },
+    Three: { title: "Equarium", solution: "A website made for Student Organisation's project at my university. Written with regular HTML/CSS/JS + Webpack & Babel", code: "https://github.com/mecies/Equarium", live: "" },
+    ThreePL: { title: "Equarium", solution: "Strona napisana dla projektu Organizacji Studenckiej na mojej uczelni w technologiach HTML/CSS/JS + Webpack i Babel", code: "https://github.com/mecies/Equarium", live: "" },
     Four: { title: "Weather App", solution: "One of my first projects written with HTML/CSS and Javascript ", code: "https://github.com/mecies/weather-app", live: "https://mecies.github.io/weather-app/", },
     FourPL: { title: "Apka Pogodowa", solution: "Jeden z moich pierwszych projektów napisany w HTML/CSS i Javascript ", code: "https://github.com/mecies/weather-app", live: "https://mecies.github.io/weather-app/", }
 }
